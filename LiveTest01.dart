48e971cb44e9ef73783e47b502230c864db67ad3
Engr. Muntasir Shajib, PMP, CISA <44524476+mshajib@users.noreply.github.com>
2025-01-01 23:09:43 +0600

    Update LiveTest01.dart

--- LiveTest01.dart
+++ LiveTest01.dart
@@ -1,1 +1,12 @@
 
+/*Write a dart program to calculate the area of the triangle. You have to call a triangle() function from the main then write your solution inside of the triangle functions. 
+Triangle area: 0.5*base*heightSubmission Guideline: must submit by github link.
+*/ 
+
+triangle(){
+
+}
+
+void main (){
+  
+}
